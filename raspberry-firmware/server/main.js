509d395df570a14d1e9b0cb99dc426e2f4e10851
Michal Conos <michal.conos@gmail.com>
2020-01-24 15:33:30 +0100

    auto-refresh via custom Vue component

--- raspberry-firmware/server/main.js
+++ raspberry-firmware/server/main.js
@@ -1,7 +1,47 @@
+Vue.component('capture', {
+    props: {
+        source: {
+            type: String,
+            required: true,
+            default: "liveview.jpg"
+        }
+    },
+    template: `
+    <div>
+    <img :src="image" />
+    </div>
+    `,
+    computed: {
+        image() {
+            return this.source+"?random="+this.currentRandom
+        }
+    },
+    data () {
+        return {
+            currentRandom: 0,
+            timer: ''
+        }
+    },
+    created () {
+        this.fetchEventsList();
+        this.timer = setInterval(this.fetchEventsList, 500)
+    },
+    methods: {
+        fetchEventsList () {
+                this.currentRandom = Math.random()
+            
+        },
+        cancelAutoUpdate () { clearInterval(this.timer) }
+
+    },
+    beforeDestroy () {
+      clearInterval(this.timer)
+    }
+});
+
 var app = new Vue({
     el: '#app',
     data: {
-        liveViewImage: 'liveview.jpg',
-	message: 'Aho'
+        liveViewImage: 'liveview.jpg'
     }
 })
